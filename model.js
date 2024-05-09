1d039f1e22974bfccb03b61235989f71651a87df
William Franklin <emailwilliamfranklin@gmail.com>
2024-05-09 15:09:02 -0700

    init

---
+++ model.js
@@ -0,0 +1,7 @@
+import * as THREE from 'https://unpkg.com/three@0.164.1/build/three.module.js';
+import { Rhino3dmLoader } from 'https://unpkg.com/three@0.164.1/examples/jsm/loaders/3DMLoader.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.164.1/examples/jsm/controls/OrbitControls.js';
+import { Tween } from 'https://unpkg.com/three@0.164.1/examples/jsm/libs/tween.module.js'
+
+console.clear();
+console.log('Hello World!')
