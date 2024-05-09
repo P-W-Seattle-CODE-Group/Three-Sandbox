bb496916d2fda30eedc8e5b1dac8b353adc2048f
William Franklin <emailwilliamfranklin@gmail.com>
2024-05-09 15:40:55 -0700

    Init of src folder and importing of initial Three objects scene, camera, renderer, etc.

--- model.js
+++ model.js
@@ -2,6 +2,8 @@ import * as THREE from 'https://unpkg.com/three@0.164.1/build/three.module.js';
 import { Rhino3dmLoader } from 'https://unpkg.com/three@0.164.1/examples/jsm/loaders/3DMLoader.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.164.1/examples/jsm/controls/OrbitControls.js';
 import { Tween } from 'https://unpkg.com/three@0.164.1/examples/jsm/libs/tween.module.js'
+import init from '/src/SceneInit.js'
 
-console.clear();
-console.log('Hello World!')
+const { scene, renderer, camera, controls } = init();
+
+
---
+++ src/SceneInit.js
@@ -0,0 +1,49 @@
+import * as THREE from 'https://unpkg.com/three@0.164.1/build/three.module.js';
+import { Rhino3dmLoader } from 'https://unpkg.com/three@0.164.1/examples/jsm/loaders/3DMLoader.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.164.1/examples/jsm/controls/OrbitControls.js';
+import { Tween } from 'https://unpkg.com/three@0.164.1/examples/jsm/libs/tween.module.js'
+
+const { scene, renderer, camera, controls } = init();
+
+function init() {
+    const scene = new THREE.Scene();
+
+    const sceneContainer = document.getElementById('scene-container');
+    const sceneWidth = sceneContainer.clientWidth;
+    const sceneHeight = sceneContainer.clientHeight;
+
+    const renderer = new THREE.WebGLRenderer(
+        {
+            antialias: true,
+            alpha: true,
+            size: {
+                width: sceneWidth,
+                height: sceneHeight
+            }
+        }
+    );
+    sceneContainer.append(renderer.domElement)
+
+    const camera = new THREE.PerspectiveCamera(
+        60,                         //FOV
+        sceneWidth / sceneHeight,   //Aspect Ratio
+        0.1,                        //Near Clipping
+        1000                        //Far Clipping
+    );
+
+    const controls = new OrbitControls(
+        camera,
+        renderer.domElement
+    );
+
+    return {
+        scene: scene,
+        renderer: renderer,
+        camera: camera,
+        controls: controls
+    }
+
+}
+init();
+
+export default init
